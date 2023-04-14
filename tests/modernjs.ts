888214fe10ff5629b1f8be56bc9eb4f5e2d2c377
ahabhgk <ahabhgk@gmail.com>
2023-04-14 14:39:43 +0800

    fix: modernjs test and use nightly for schedule

--- tests/modernjs.ts
+++ tests/modernjs.ts
@@ -6,6 +6,9 @@ export async function test(options: RunOptions) {
 		...options,
 		repo: 'web-infra-dev/modern.js',
 		branch: 'main',
+		beforeTest: [
+			'cd tests/e2e/builder && pnpm playwright install chromium && cd ../../../',
+		],
 		test: ['test:rspack'],
 	})
 }
