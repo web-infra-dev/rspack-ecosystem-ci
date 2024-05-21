fb618dc51fcb41b97140276499dda5f4e1caadc6
Cong-Cong <dacongsama@live.com>
2024-05-21 16:27:25 +0800

    fix: no __dirname

--- tests/modernjs.ts
+++ tests/modernjs.ts
@@ -4,25 +4,35 @@ import { join } from 'node:path'
 import { runInRepo, cd, $ } from '../utils'
 import { RunOptions } from '../types'
 
+const isGitHubActions = !!process.env.GITHUB_ACTIONS
+
 function getCurrentCommitSha() {
 	return execSync('git rev-parse HEAD').toString().trim()
 }
 
 export async function test(options: RunOptions) {
-	const nxCachePath = join(__dirname, 'workspace/modernjs/modern.js')
-	let nxCacheKey = 'modernjs-nx-'
+	let nxCachePath: string
+	let nxCacheKey: string
 
 	await runInRepo({
 		...options,
 		repo: 'web-infra-dev/modern.js',
 		branch: process.env.MODERN_REF ?? 'main',
 		beforeInstall: async () => {
-			nxCacheKey += getCurrentCommitSha()
-			const restoreKeys = ['modernjs-nx-']
-			await cache.restoreCache([nxCachePath], nxCacheKey, restoreKeys)
+			if (isGitHubActions) {
+				nxCachePath = join(
+					process.cwd(),
+					'workspace/modernjs/modern.js/.nx/cache',
+				)
+				nxCacheKey = 'modernjs-nx-' + getCurrentCommitSha()
+				const restoreKeys = ['modernjs-nx-']
+				await cache.restoreCache([nxCachePath], nxCacheKey, restoreKeys)
+			}
 		},
 		afterInstall: async () => {
-			await cache.saveCache([nxCachePath], nxCacheKey)
+			if (isGitHubActions) {
+				await cache.saveCache([nxCachePath], nxCacheKey)
+			}
 		},
 		beforeTest: async () => {
 			cd('tests/e2e/builder')
