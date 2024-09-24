d127030d6bebd1ce3a93ae7b0a808b886a5d9d36
LingyuCoder <lingyucoder@gmail.com>
2024-09-24 12:59:02 +0800

    feat: add rspack-dev-server eco ci

--- tests/devserver.ts
+++ tests/devserver.ts
@@ -2,10 +2,10 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
-  await runInRepo({
-    ...options,
-    repo: 'web-infra-dev/rspack-dev-server',
-    branch: 'main',
-    test: ['test'],
-  })
+	await runInRepo({
+		...options,
+		repo: 'web-infra-dev/rspack-dev-server',
+		branch: 'main',
+		test: ['test'],
+	})
 }
