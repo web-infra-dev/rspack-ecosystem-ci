bf332e35a670ccfd8bcd286d6ae157d7dadf41d4
LingyuCoder <lingyucoder@gmail.com>
2024-09-27 19:01:49 +0800

    feat: upgrade nx ci

--- tests/nx.ts
+++ tests/nx.ts
@@ -8,10 +8,10 @@ export async function test(options: RunOptions) {
 		branch: 'master',
 		beforeTest: async () => {
 			await $`cargo build`
-			await $`NX_DAEMON=false pnpm run build --skip-nx-cache`
+			await $`pnpm run build --skip-nx-cache`
 		},
 		test: [
-			'NX_DAEMON=false pnpm nx test --skip-nx-cache rspack',
+			'pnpm nx test --skip-nx-cache rspack',
 			// 'pnpm nx run-many -t e2e-local -p e2e-rspack --skip-nx-cache',
 		],
 	})
