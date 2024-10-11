7d3e0ac2fe55edb12b5d538e0f944fea41393766
Cong-Cong <dacongsama@live.com>
2024-10-11 14:09:21 +0800

    fix: playwright install woring in rsbuild

--- tests/rsbuild.ts
+++ tests/rsbuild.ts
@@ -8,7 +8,7 @@ export async function test(options: RunOptions) {
 		branch: process.env.RSBUILD_REF ?? 'main',
 		beforeTest: async () => {
 			cd('./e2e')
-			await $`pnpm playwright install --with-deps`
+			await $`npx playwright install`
 			cd('..')
 		},
 		test: ['e2e:rspack'],
