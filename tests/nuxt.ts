2c4f14fd7a8ed8c4e5b771238935cc924595ba76
sunyiteng <sunyiteng@bytedance.com>
2024-11-06 18:13:45 +0800

    feat: add nuxt to eco-ci

---
+++ tests/nuxt.ts
@@ -0,0 +1,16 @@
+import { runInRepo, $ } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'nuxt/nuxt',
+		branch: 'main',
+		beforeTest: async () => {
+			await $`pnpm playwright-core install chromium`
+		},
+		test: [
+			'TEST_ENV=built TEST_BUILDER=rspack TEST_MANIFEST=manifest-on TEST_CONTEXT=async TEST_PAYLOAD=json SKIP_BUNDLE_SIZE=true pnpm test:fixtures',
+		],
+	})
+}
