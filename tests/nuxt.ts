8e0a1d78b508d7af77c0fc9d70a7d85df6dcea55
sunyiteng <sunyiteng@bytedance.com>
2024-11-08 12:08:43 +0800

    chore: update

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -1,33 +1,25 @@
 import { runInRepo, $ } from '../utils'
 import { RunOptions } from '../types'
 
-async function restoreProcessEnv(task: () => Promise<any>) {
-	const initialProcessEnv = process.env
-	process.env = {
-		...initialProcessEnv,
-		TEST_ENV: 'built',
-		TEST_BUILDER: 'rspack',
-		TEST_MANIFEST: 'manifest-on',
-		TEST_CONTEXT: 'async',
-		TEST_PAYLOAD: 'json',
-		SKIP_BUNDLE_SIZE: 'true',
-	}
-	await task()
-	process.env = initialProcessEnv
-}
-
+let initialProcessEnv;
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'nuxt/nuxt',
 		branch: 'main',
 		beforeTest: async () => {
+			initialProcessEnv = process.env;
+			process.env = {
+				...initialProcessEnv,
+				TEST_ENV: 'built',
+				TEST_BUILDER: 'rspack',
+				TEST_MANIFEST: 'manifest-on',
+				TEST_CONTEXT: 'async',
+				TEST_PAYLOAD: 'json',
+				SKIP_BUNDLE_SIZE: 'true',
+			}
 			await $`pnpm playwright-core install chromium`
 		},
-		test: async () => {
-			await restoreProcessEnv(async () => {
-				await $`pnpm test:fixtures`
-			})
-		},
+		test: ['test:fixtures'],
 	})
 }
