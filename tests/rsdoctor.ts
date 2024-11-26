26aeb723c73cb7aba30c0295aefad5707e47050d
easy1090 <congzhe@bytedance.com>
2024-11-26 16:48:47 +0800

    feat: add rsdoctor in rspack ecosystem-ci

---
+++ tests/rsdoctor.ts
@@ -0,0 +1,16 @@
+import { runInRepo, $, cd } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'web-infra-dev/rsdoctor',
+		branch: process.env.RSDOCTOR ?? 'main',
+		beforeTest: async () => {
+			cd('./e2e')
+			await $`pnpm playwright install --with-deps`
+			cd('..')
+		},
+		test: ['e2e'],
+	})
+}
